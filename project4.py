f163647e3b45306d157c0ed08014f8001fd69fc7
Imaan Munir <imunir@umich.edu>
2017-11-30 14:46:50 -0500

    descriptions for APIS

--- project4.py
+++ project4.py
@@ -1,3 +1,6 @@
-# Gmail
-# Facebook
-# Google Maps
+#Imaan Munir - Project 4
+
+
+# Gmail - see what days/times I am most active on
+# Facebook - see when I like posts to determine what days/times I am most active on
+# Google Maps - see what day/time Nobu has the longest waits for
